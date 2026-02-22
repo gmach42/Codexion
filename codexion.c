c3668ad49a9ecefbf3dde32d7cdbf9783da82797
gmach <gmach@student.42lyon.fr>
2026-02-22 19:23:36 +0100

    save up init done and begin routine

--- codexion.c
+++ codexion.c
@@ -1,50 +1,11 @@
 #include "codexion.h"
 
-// AUTHORIZED :
-// pthread_create, pthread_join, pthread_mutex_init,
-// pthread_mutex_lock,
-// pthread_mutex_unlock, pthread_mutex_destroy,
-// pthread_cond_init,
-// pthread_cond_wait, pthread_cond_timedwait,
-// pthread_cond_broadcast,
-// pthread_cond_destroy, gettimeofday, usleep, write,
-// malloc, free,
-// printf, fprintf, strcmp, strlen, atoi, memset
-
-
-// ARGS :
-// number_of_coders time_to_burnout time_to_compile time_to_debug
-// time_to_refactor number_of_compiles_required dongle_cooldown scheduler
-
-void* print_id(void* arg) {
-	int id = *(int *)arg;
-    printf("Hello from thread %d!\n", id);
-    return NULL;
-}
-
-void* create_coder_thread(pthread_t *threads, int nb_coder)
-{
-	int i;
-
-	i = 1;
-	int ids[nb_coder];
-
-	while (i <= nb_coder)
-	{
-		ids[i] = i;
-		pthread_create(&threads[i], NULL, print_id, &ids[i]);
-		i++;
-	}
-	return threads;
-}
-
 int main(int argc, char **argv)
 {
-
-	(void)argc;
-	(void)argv;
-
 	t_simulation	simulation;
+	t_coder			*coders;
+	t_dongle		*dongles;
+	pthread_t		monitor_thread;
 
 	if (argc != 9)
 	{
@@ -52,7 +13,67 @@ int main(int argc, char **argv)
 		return (0);
 	}
 
-	
+	simulation = simulation_init(argv);
+	dongles = dongles_init(simulation);
+	coders = coders_init(simulation);
+	monitor_thread = monitor_init(simulation);
+
+	/**
+	 * @brief Each coders thread routine will try to take the left and right dongle, compile, debug and refactor until burnout
+	 * The routine will end when the coder has compiled the required number of times or is burned
+	 */
+	while (monitor_thread)
+		routine(simulation, coders);
+
+	destroy(simulation, coders, dongles, monitor_thread);
 
     return 0;
+}
+
+void routine(t_simulation simulation, t_coder *coder)
+{
+	while (!is_burned(simulation) || has_compiled_enough(simulation))
+	{
+		try_to_take_dongles(simulation, coder);
+		compile(simulation, coder);
+		debug(simulation, coder);
+		refactor(simulation, coder);
+	}
+}
+
+// Check if the coder has been burned out based on the time since last compile and the burnout threshold
+bool is_burned(t_simulation simulation)
+{
+	struct timeval now;
+
+	gettimeofday(&now, NULL);
+    double time_passed;
+    time_passed = (now.tv_sec - simulation.start_time.tv_sec) * 1e6;
+    time_passed = (time_passed + (now.tv_usec - simulation.start_time.tv_usec)) * 1e-6;
+	return (time_passed > simulation.time_to_burnout)
+}
+
+bool has_compiled_enough(t_simulation simulation)
+{
+	return (simulation.number_of_compiles >= simulation.number_of_compiles_required);
+}
+
+try_to_take_dongles(t_simulation simulation, t_coder *coder)
+{
+	// Attempt to take the left and right dongle, waiting if necessary until they are available
+}
+
+compile(t_simulation simulation, t_coder *coder)
+{
+	// Simulate the compile action, update coder's state and release dongles after use
+}
+
+debug(t_simulation simulation, t_coder *coder)
+{
+	// Simulate the debug action, update coder's state
+}
+
+refactor(t_simulation simulation, t_coder *coder)
+{
+	// Simulate the refactor action, update coder's state
 }
--- codexion.h
+++ codexion.h
@@ -1,23 +1,29 @@
 #ifndef CODEXION_H
 # define CODEXION_H
 
-# include <unistd.h>
-# include <stdio.h>
-# include <pthread.h>
-#include <sys/time.h>
+# include <unistd.h> // usleep, write
+# include <stdio.h> // printf, fprintf
+# include <pthread.h> // pthread functions
+# include <sys/time.h> // gettimeofday
+# include <stdlib.h> // malloc, free, atoi
+# include <string.h> // memset
+# include <limits.h> // INT_MAX, INT_MIN
+# include <stdbool.h> // bool, true, false
 
 typedef struct s_simulation
 {
-	t_coder		*coders;
-	t_dongle	*dongles;
-	int			number_of_coders;
-	int			time_to_burnout;
-	int			time_to_compile;
-	int			time_to_debug;
-	int			time_to_refactor;
-	int			number_of_compiles_required;
-	int			dongle_cooldown;
-	char		*scheduler;
+	t_coder			*coders;
+	t_dongle		*dongles;
+	int				number_of_coders;
+	int				time_to_burnout;
+	int				time_to_compile;
+	int				time_to_debug;
+	int				time_to_refactor;
+	int				number_of_compiles;
+	int				number_of_compiles_required;
+	int				dongle_cooldown;
+	char			*scheduler;
+	struct timeval	start_time;
 }	t_simulation;
 
 /**
@@ -48,7 +54,7 @@ typedef struct s_coder
  *
  * @param	mutex		Lock a dongle when it's used
  * @param	cond		Condition to lock the dongle (cooldown)
- * @param	available	1 is free, 0 is taken
+ * @param	available	True if the dongle is available, false if it's being used or in cooldown
  * @param	cooldown	Timestamp in ms of the cooldown
  * @param	schedule	Heapq storing coders for each dongle
 */
@@ -57,7 +63,7 @@ typedef struct s_dongle
 	pthread_mutex_t	mutex;	/**< Protects access to dongle state */
 	pthread_cond_t	cond;	/**< Signal waiting coders */
 
-	int				available;	/**< 1 is free, 0 is taken */
+	bool			available;	/**< Boolean indicating if the dongle is available */
 	long			cooldown;	/**< Timestamp (ms) when dongle is reusable */
 
 	t_heapq			*schedule; /**< Heapq of waiting coders */
@@ -78,15 +84,36 @@ typedef struct s_heapq
 	int		deadline; /**< deadline = last_compile_start + time_to_burnout */
 }	t_heapq;
 
-/* Coder functions */
-t_coder	coder_init(t_coder	*coders);
-// Try to take dongle
-//
+/* Init functions */
+t_coder			coder_init(t_coder	*coders);
+t_coder			*coders_init(t_simulation simulation);
+t_dongle		dongle_init(t_simulation simulation);
+t_dongle		*dongles_init(t_simulation simulation);
+t_simulation	simulation_init(char **argv);
+pthread_t		monitor_init(t_simulation simulation);
 
 /* Dongle functions */
-t_dongle	dongle_init(int dongle_cooldown, t_heapq schedule);
 int			can_take_dongle(t_coder *coder, t_dongle *dongle);
-void		try_to_take_dongle(t_dongle *dongle, t_coder *coder);
-void		cleanup_dongle(t_dongle **dongle);
+void		take_dongle(t_coder *coder, t_dongle *dongle);
+void		release_dongle(t_coder *coder, t_dongle *dongle);
+void		try_to_take_dongles(t_simulation simulation, t_coder *coder);
+void		update_dongle_cooldown(t_dongle *dongle);
+
+/* Coder functions */
+void		routine(t_simulation simulation, t_coder *coder);
+bool		is_burned(t_simulation simulation);
+bool 		has_compiled_enough(t_simulation simulation);
+void		compile(t_simulation simulation, t_coder *coder);
+void		debug(t_simulation simulation, t_coder *coder);
+void		refactor(t_simulation simulation, t_coder *coder);
+
+/* Monitor functions */
+void		*monitor_routine(void *arg);
+
+/* Destroy functions */
+void		destroy(t_simulation simulation, t_coder *coders, t_dongle *dongles, pthread_t monitor_thread);
+void		destroy_coders(t_coder *coders, int number_of_coders);
+void		destroy_dongles(t_dongle *dongles, int number_of_coders);
+void		destroy_monitor(pthread_t monitor_thread);
 
 #endif
